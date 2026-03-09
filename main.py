262c6135ae98cfbcdc6fa18fc8318ae499d432ef
Grigorii Kuznetsov <grishazaordu@mail.ru>
2026-03-09 19:16:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ import os
 import requests
 import streamlit as st
 
-from design import display_logo
-from design_2 import setup_style, display_header, init_sidebar, render_librarian_mode, render_analyst_mode
+from design import setup_style, display_header, init_sidebar, render_librarian_mode, render_analyst_mode
 from config import Config
 from session_state import init_session_state
 
 import sys
-from pathlib import Path
 
-# Добавляем путь к shared модулям
-sys.path.append(str(Path(__file__).parent.parent))
 
-def app():
+def main():
 
     st.set_option('client.showErrorDetails', False)
 
@@ -21,27 +17,6 @@ def app():
 
     # Инициализация состояния сессии
     init_session_state()
-
-    # Проверка авторизации
-    # query_params = st.experimental_get_query_params()
-
-    # if 'code' not in query_params:
-    #     # Формируем URL для авторизации
-    #     auth_url = (
-    #         f'{Config.KEYCLOAK_AUTH_URL}/realms/{Config.KEYCLOAK_REALM}/'
-    #         f'protocol/openid-connect/auth?client_id={Config.KEYCLOAK_CLIENT_ID}&'
-    #         f'redirect_uri={Config.KEYCLOAK_REDIRECT_URI}&response_type=code&scope=openid'
-    #     )
-    #
-    #     # Автоматический редирект
-    #     st.markdown(f"""<meta http-equiv="refresh" content="0; url='{auth_url}'" />""", unsafe_allow_html=True)
-    # else:
-    #
-    #     setup_style()
-        # st.success("✅ Успешная аутентификация!")
-
-    # display_logo()
-    # display_header()
     init_sidebar()
 
     if st.session_state.mode == 'analyst':
@@ -50,7 +25,7 @@ def app():
         render_librarian_mode()
 
     # Отображение интерфейса
-    # render_main_interface()
+    render_main_interface()
 
 def render_main_interface():
     """Основной интерфейс приложения"""
@@ -60,18 +35,6 @@ def render_main_interface():
 
     import sys
     from pathlib import Path
-
-    # Добавляем путь к shared модулям
-    sys.path.append(str(Path(__file__).parent.parent))
-
-    # Скачивание Word инструкции
-    with open('streamlit-app/Инструкция_listingai.docx', 'rb') as file:
-        st.download_button(
-            label='📜 Скачать инструкцию по работе с приложением',
-            data=file.read(),
-            file_name='instruction.docx',
-            mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        )
 
     # Выбор проверок
     selected_checks = show_check_selector()
@@ -96,4 +59,4 @@ def render_main_interface():
             check['function']()
 
 if __name__ == "__main__":
-    app()
+    main()
